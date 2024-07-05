d2b568e76ee215a462d80cc05ce26d6bc2c3bc09
Austin <austin@flipsidecrypto.com>
2024-07-05 08:49:54 -0400

    20k

--- models/streamline/realtime/streamline__testnet_receipts_realtime.sql
+++ models/streamline/realtime/streamline__testnet_receipts_realtime.sql
@@ -4,8 +4,8 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"testnet_receipts",
-        "sql_limit" :"25000",
-        "producer_batch_size" :"6000",
+        "sql_limit" :"20000",
+        "producer_batch_size" :"5000",
         "worker_batch_size" :"2000",
         "sql_source" :"{{this.identifier}}",
         "exploded_key": tojson(["result"]) }
@@ -62,4 +62,4 @@ SELECT
         ORDER BY
             block_number DESC
         LIMIT
-            25000
+            20000
