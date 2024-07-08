7b2c1d772edd61097a32084f894cbc95f17d1855
drethereum <trevor.wenokur@gmail.com>
2024-07-08 11:18:38 -0600

    silver models

---
+++ models/silver/core/tests/blocks/test_silver__blocks_recent.sql
@@ -0,0 +1,24 @@
+{{ config (
+    materialized = 'view',
+    tags = ['recent_test']
+) }}
+
+WITH last_3_days AS (
+
+    SELECT
+        block_number
+    FROM
+        {{ ref("_block_lookback") }}
+)
+
+SELECT
+    *
+FROM
+    {{ ref('silver_testnet__blocks') }}
+WHERE
+    block_number >= (
+        SELECT
+            block_number
+        FROM
+            last_3_days
+    )
