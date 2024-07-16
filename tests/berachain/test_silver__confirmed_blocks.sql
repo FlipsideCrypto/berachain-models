f1a33a936ad3b77be5b3f1398dc4d925a55d1928
drethereum <trevor.wenokur@gmail.com>
2024-07-16 16:39:41 -0600

    comment

--- tests/berachain/test_silver__confirmed_blocks.sql
+++ tests/berachain/test_silver__confirmed_blocks.sql
@@ -1,1 +1,1 @@
-{{ missing_confirmed_txs(ref("test_silver__confirmed_blocks_full"), ref("test_silver__transactions_full")) }}
+{# {{ missing_confirmed_txs(ref("test_silver__confirmed_blocks_full"), ref("test_silver__transactions_full")) }} #}
